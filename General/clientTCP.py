4afd66d181c5436489116682d08fc039f3fefd52
Charin Ivan <lumidelta07@gmail.com>
2023-04-04 22:17:07 +0300

    Add TCP client/server

---
+++ General/clientTCP.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host="127.0.0.1"
+target_port=9997
+
+# create client socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to host
+client.connect((target_host, target_port))
+
+# send data
+client.send(b"AAABBBCCC")
+
+# get responce
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
