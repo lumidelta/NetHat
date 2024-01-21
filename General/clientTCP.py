3df47a4a09a1fc763e4e878d8cfad90e0cc5da29
Ivan Charin <lyambdadelta86@gmail.com>
2024-01-21 16:51:38 +0300

    fix mistake & send twice

--- General/clientTCP.py
+++ General/clientTCP.py
@@ -10,7 +10,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send data
-client.send(b"something");
+client.send(b"something")
+client.send(b"something")
 
 # get responce
 response = client.recv(4096)
